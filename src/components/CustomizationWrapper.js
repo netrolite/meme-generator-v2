e4bf9cb1653c665a9c92fc31f106c83afd53dfa8
matvey733 <morozovmatvej75@gmail.com>
2022-10-28 18:57:18 +0300

    Reverted

--- src/components/CustomizationWrapper.js
+++ src/components/CustomizationWrapper.js
@@ -30,65 +30,6 @@ export default function CustomizationWrapper(props) {
                     Apply
                     </button>
                 </form>
-
-                <label> 
-                    <input
-                        type="checkbox"
-                        name="isLegal"
-                        checked={props.isChecked}
-                        onChange={props.handleChange}
-                        />                
-                        Is it legal? 
-                </label>
-
-                <fieldset>
-                    <legend>Your employment status</legend>
-
-                    <div>
-                        <input 
-                            type="radio"
-                            name="employment"
-                            id="employmentChoice1"
-                            value="unemployed"
-                            onChange={props.handleChange}
-                        />
-                        <label htmlFor="employmentChoice1">Employed full-time</label>
-                    </div>
-
-                    <div>
-                        <input 
-                            type="radio"
-                            name="employment"
-                            id="employmentChoice2"
-                            value="part-time"
-                            onChange={props.handleChange}
-                        />
-                        <label htmlFor="employmentChoice2">Employed part-time</label>
-                    </div>
-
-                    <div>
-                        <input 
-                            type="radio"
-                            name="employment"
-                            id="employmentChoice3"
-                            value="full-time"
-                            onChange={props.handleChange}
-                        />
-                        <label htmlFor="employmentChoice3">Employed full-time</label>
-                    </div>
-                        
-                    <div>
-                        <input 
-                            type="radio"
-                            name="employment"
-                            id="employmentChoice4"
-                            value="business"
-                            onChange={props.handleChange}
-                        />
-                        <label htmlFor="employmentChoice4">Running a business</label>
-                    </div>
-
-                </fieldset>
             </div>    
     )
 }
